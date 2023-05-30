c300141a91c7ae3d9229622a31a906c3b1fc83bc
agustin cordero <agustin.cordero201@gmail.com>
2023-05-30 17:05:20 -0300

    first commit

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import { Navigation } from "./components/Navigation"
+
+import '../styles/globals.css'
+
+export default function RootLayout({ children }) {
+ return (
+    <html lang="en">
+      <head>
+          <title>My first next 13 app</title>
+      </head>
+      <body>
+        <Navigation/>
+        {children}
+      </body>
+    </html>
+  )
+}
